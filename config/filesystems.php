feb43bc9eb2df3dfef6ec03ba6eeafd48f1e2336
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-02-28 00:24:53 +0500

    laravel new project added..

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,7 +37,7 @@ return [
     | may even configure multiple disks of the same driver. Defaults have
     | been setup for each driver as an example of the required options.
     |
-    | Supported Drivers: "local", "ftp", "sftp", "s3", "rackspace"
+    | Supported Drivers: "local", "ftp", "sftp", "s3"
     |
     */
 
@@ -45,7 +45,7 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => public_path('image/'),
+            'root' => storage_path('app'),
         ],
 
         'public' => [
--- routes/web.php
+++ routes/web.php
@@ -11,33 +11,6 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-// Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/', 'BlogController@index');
-Route::get('/blog/show/{id}', 'BlogController@show');
-
-//Route::group(['middleware' => ['web']],function(){
-// Route::resource('post','BlogController');
-// Route::POST('addPost','BlogController@addPost');
-// Route::POST('editPost','BlogController@editPost');
-// Route::POST('deletePost','BlogController@deletePost');
-//});
-
-// Auth::routes();
-Auth::routes(['register' => false]);
-Route::group(['middleware' => 'auth'], function() {
-    Route::get('/admin','AdminController@index');
-    Route::get('/admin/create/post','AdminController@create');
-    Route::post('/admin/store/post','AdminController@store');
-    Route::get('/admin/edit/post/{id}','AdminController@edit');
-    Route::post('/admin/update/post/{id}','AdminController@update');
-    Route::get('/admin/delete/post/{id}','AdminController@destroy');
-    Route::get('/admin/settings/{id}','AdminController@showsettings');
-    Route::post('/admin/settings/{id}','AdminController@settings');
+Route::get('/', function () {
+    return view('welcome');
 });
-
-
-
