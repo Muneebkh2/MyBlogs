3436da6ef0e08f3c0ab992e312e12a68c685ae28
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-02-28 02:35:07 +0500

    home template updated.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+<!-- Page Header -->
+<div class="page-header row no-gutters py-4">
+    <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
+        <span class="text-uppercase page-subtitle">Dashboard</span>
+        <h3 class="page-title">MyBlogs > Dashboard</h3>
     </div>
 </div>
+<!-- End Page Header -->
+
+@if (session('status'))
+<div class="alert alert-success alert-dismissible fade show mb-0" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">×</span>
+    </button>
+    <i class="fa fa-check mx-2"></i>
+    <strong>Success!</strong> 
+    {{ session('status') }}
+</div>
+@endif
+
+<p>
+    You are logged in!
+</p>   
 @endsection
